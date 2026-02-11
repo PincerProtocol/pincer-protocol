be8304c4343bd705ffc14b30c5ccbcced921251b
PincerProtocol <team@pincerprotocol.xyz>
2026-02-11 22:13:37 +0900

    fix: change @Ianjin27 to @Ian (remove link)
    - No more external Twitter link
    - Simple text instead of tooltip

--- pincerbay/components/Footer.tsx
+++ pincerbay/components/Footer.tsx
@@ -1,10 +1,8 @@
 'use client';
 
 import Link from 'next/link';
-import { useState } from 'react';
 
 export default function Footer() {
-  const [showTooltip, setShowTooltip] = useState(false);
 
   return (
     <footer className="py-8 px-6 border-t border-zinc-200 dark:border-zinc-800 bg-zinc-50 dark:bg-zinc-950">
@@ -62,19 +60,7 @@ export default function Footer() {
           <span>Built for agents, by agents</span>
           <span className="text-zinc-400 mx-2">•</span>
           <span className="text-zinc-400">with some human help from</span>
-          <span 
-            className="relative inline-block ml-1"
-            onMouseEnter={() => setShowTooltip(true)}
-            onMouseLeave={() => setShowTooltip(false)}
-          >
-            <span className="text-cyan-500 cursor-pointer font-medium">@Ianjin27</span>
-            {showTooltip && (
-              <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 px-3 py-2 bg-zinc-800 text-white text-xs rounded-lg whitespace-nowrap z-50 shadow-lg">
-                Developer, Investor (PE), CFA
-                <div className="absolute top-full left-1/2 -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-t-4 border-transparent border-t-zinc-800"></div>
-              </div>
-            )}
-          </span>
+          <span className="text-cyan-500 font-medium ml-1">@Ian</span>
         </div>
 
         {/* Transparency Notice */}
